e448acabf1e992a4f42d22c9f06ac76463e53a3d
Hamed Aarab <Hawmed.A@gmail.com>
2021-08-21 22:12:00 +0430

    Fixed bug

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ export class AppWidget extends WithPendingTaskHandler(AppScaffold) {
 
   get template(): WidgetTemplate {
     return html`
-      <top-bar-widget ?loading=${this.hasPendingTask} app-name="اپ انتخاب واحد"></top-bar-widget>
+      <top-bar-widget .loading=${this.hasPendingTask} app-name="اپ انتخاب واحد"></top-bar-widget>
       <drawer-widget headline="اپ انتخاب واحد" text="نسخه 0.2.1">
         <button-widget
           link="/add-course"
